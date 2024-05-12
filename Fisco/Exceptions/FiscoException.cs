27e51fd5380cf61da06399c950c80653d25e2ab8
Manoel Victor Silva Lira <manoelvictorzxc17@hotmail.com>
2024-05-12 20:12:31 -0300

    Fix

--- Fisco/Exceptions/FiscoException.cs
+++ Fisco/Exceptions/FiscoException.cs
@@ -8,5 +8,15 @@ namespace Fisco.Exceptions
         {
 
         }
+
+        public FiscoException(string message, Exception exception) : base(message, exception)
+        {
+
+        }
+
+        public FiscoException()
+        {
+            
+        }
     }
 }
